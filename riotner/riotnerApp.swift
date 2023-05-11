9542739782a598ba05d0ac9c9eb8507721f77490
Eris Ramirez <kowayasky@gmail.com>
2023-05-11 12:20:18 -0500

    Initial Commit

---
+++ riotner/riotnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  riotnerApp.swift
+//  riotner
+//
+//  Created by Eris Ramirez on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct riotnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
