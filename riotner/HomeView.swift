ca6f6ea51538cb75fcfc55ec6678bf6962b3356d
Eris Ramirez <kowayasky@gmail.com>
2023-05-15 22:56:44 -0500

    implement home section skeleton

---
+++ riotner/HomeView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeView.swift
+//  riotner
+//
+//  Created by Eris Ramirez on 15/05/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    private var sections = [1,2,3,4]
+    var body: some View {
+        VStack {
+            List {
+                ForEach(sections, id: \.self) { section in
+                    Section {
+                        switch section {
+                        case 4: Text("Challenger tier list")
+                        default: Text("Section \(section)")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
